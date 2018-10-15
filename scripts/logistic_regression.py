90c4861135343547982128071866b30f33c73fb5
yongbiaoai <yongbiaoai@gmail.com>
2018-10-15 10:57:26 +0800

    modify logistic_regression script and change cluster node port
    Signed-off-by: yongbiaoai <yongbiaoai@gmail.com>

--- scripts/logistic_regression.py
+++ scripts/logistic_regression.py
@@ -36,10 +36,12 @@ env_params = (
     "GLOG_logtostderr=true "
     "GLOG_v=-1 "
     "GLOG_minloglevel=0 "
-    # this is to enable hdfs short-circuit read (disable the warning info)
-    # change this path accordingly when we use other cluster
-    # the current setting is for proj5-10
-    "" if local_debug else "LIBHDFS3_CONF=/data/opt/course/hadoop/etc/hadoop/hdfs-site.xml"
 )
+
+# this is to enable hdfs short-circuit read (disable the warning info)
+# change this path accordingly when we use other cluster
+# the current setting is for proj5-10
+if (local_debug is False):
+    env_params += "LIBHDFS3_CONF=/data/opt/course/hadoop/etc/hadoop/hdfs-site.xml"
 
 launch_util(progfile, hostfile, env_params, params, sys.argv)
