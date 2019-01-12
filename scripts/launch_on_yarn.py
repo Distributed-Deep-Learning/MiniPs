703924962a075c8c9e699f2646480e38602796d5
yongbiaoai <yongbiaoai@gmail.com>
2019-01-12 12:20:30 +0800

    change yarn script
    Signed-off-by: yongbiaoai <yongbiaoai@gmail.com>

--- scripts/launch_on_yarn.py
+++ scripts/launch_on_yarn.py
@@ -9,7 +9,7 @@ app_dir = dirname(dirname(os.path.realpath(__file__)))
 proj_dir = dirname(dirname(app_dir))
 
 params = {
-    "jar": join(app_dir, "yarn/build/libs/yarn-0.5.jar")
+    "jar": join(app_dir, "utils/yarn-0.5.jar")
     , "launch_script_path": join(app_dir, "scripts/logistic_regression.py")
     , "container_memory": 500
     , "container_vcores": 1
@@ -19,7 +19,7 @@ params = {
 }
 
 cmd = "hadoop jar "
-cmd += join(app_dir, "yarn/build/libs/yarn-0.5.jar") + " "
+cmd += join(app_dir, "utils/yarn-0.5.jar") + " "
 cmd += "cn.edu.buaa.act.petuumOnYarn.Client"
 cmd += "".join([" --%s %s" % (k,v) for k,v in params.items()])
 print cmd
