ca9221123296055fc3aaa492c9667c7e3810c8af
yongbiaoai <yongbiaoai@gmail.com>
2019-01-16 17:58:36 +0800

    change loader num
    Signed-off-by: yongbiaoai <yongbiaoai@gmail.com>

--- scripts/yarn/remote/yarn_example.py
+++ scripts/yarn/remote/yarn_example.py
@@ -37,7 +37,7 @@ params = {
     "batch_size": 1,
     "num_workers_per_node": 2,
     "num_servers_per_node": 1,
-    "num_local_load_thread": 2 if local_debug else 100,
+    "num_local_load_thread": 2 if local_debug else 2,
     "num_iters": 100,
     "alpha": 0.1,  # learning rate
     "with_injected_straggler": 1,  # {0/1}
